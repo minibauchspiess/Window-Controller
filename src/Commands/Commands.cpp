9994bf15f109088d416965b877817f9cf2c30119
Daniel Bauchspiess <dbauchspiess@gmail.com>
2023-03-05 09:36:11 -0300

    Passed dummy functions to Commands class. They'll be deleted shortly

--- src/Commands/Commands.cpp
+++ src/Commands/Commands.cpp
@@ -1,8 +1,23 @@
 #include "Commands/Commands.h"
 
+void dummyFunctionOne(String value){
+    Serial.printf("In first dummy function with value %s\n", value.c_str());
+}
+
+void dummyFunctionTwo(String value){
+    Serial.printf("In second dummy function with value %s\n", value.c_str());
+}
+
+void dummyFunctionThree(String value){
+    Serial.printf("In third dummy function with value %s\n", value.c_str());
+}
+
 
 Commands::Commands()
 {
+    _commandFunctionsMap["One"] = dummyFunctionOne;
+    _commandFunctionsMap["Two"] = dummyFunctionTwo;
+    _commandFunctionsMap["Three"] = dummyFunctionThree;
 }
 
 Commands::~Commands()
