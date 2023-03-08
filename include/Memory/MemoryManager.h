f9c79d3bd8197183084bb9d88004deb966704af3
Daniel Bauchspiess <dbauchspiess@gmail.com>
2023-03-08 14:38:29 -0300

    Wifi and azure iot configurations are now written and read from memory

--- include/Memory/MemoryManager.h
+++ include/Memory/MemoryManager.h
@@ -12,6 +12,12 @@ public:
     
     static void WriteStringOnFile(String filename, String fileContent);
     static String ReadStringFromFile(String fileName);
+
+    static const constexpr char *WIFI_SSID_FILENAME = "/WifiSSID.txt";
+    static const constexpr char *WIFI_PASSWORD_FILENAME = "/WifiPassword.txt";
+    static const constexpr char *SCOPE_FILENAME = "/Scope.txt";
+    static const constexpr char *DEVICE_FILENAME = "/Device.txt";
+    static const constexpr char *KEY_FILENAME = "/Key.txt";
 private:
 };
 
--- src/AzureCommunication/AzureCommunication.cpp
+++ src/AzureCommunication/AzureCommunication.cpp
@@ -12,11 +12,6 @@ char AzureCommunication::_deviceKey[100];
 
 AzureCommunication::AzureCommunication()
 {
-  SetWifiSsid("***");
-  SetWifiPassword("***");
-  SetScopeId("***");
-  SetDeviceId("***");
-  SetDeviceKey("***");
 }
 
 AzureCommunication::~AzureCommunication()
@@ -25,6 +20,12 @@ AzureCommunication::~AzureCommunication()
 
 
 void AzureCommunication::Setup() {
+
+  SetWifiSsid(MemoryManager::ReadStringFromFile(MemoryManager::WIFI_SSID_FILENAME));
+  SetWifiPassword(MemoryManager::ReadStringFromFile(MemoryManager::WIFI_PASSWORD_FILENAME));
+  SetScopeId(MemoryManager::ReadStringFromFile(MemoryManager::SCOPE_FILENAME));
+  SetDeviceId(MemoryManager::ReadStringFromFile(MemoryManager::DEVICE_FILENAME));
+  SetDeviceKey(MemoryManager::ReadStringFromFile(MemoryManager::KEY_FILENAME));
 
   _connection.ConnectWifi(_wifiSsid, _wifiPassword);
   if(_connection.isConnectedToWifi())
@@ -86,26 +87,31 @@ void AzureCommunication::SetCommandFunctionsMap(std::map<String, void(*)(String)
 
 void AzureCommunication::SetWifiSsid(String ssid){
   strcpy(_wifiSsid, ssid.c_str());
+  MemoryManager::WriteStringOnFile(MemoryManager::WIFI_SSID_FILENAME, ssid);
   Serial.printf("Updated wifi ssid to %s\n", _wifiSsid);
 }
 
 void AzureCommunication::SetWifiPassword(String password){
   strcpy(_wifiPassword, password.c_str());
+  MemoryManager::WriteStringOnFile(MemoryManager::WIFI_PASSWORD_FILENAME, password);
   Serial.printf("Updated wifi password to %s\n", _wifiPassword);
 }
 
 void AzureCommunication::SetScopeId(String scopeId){
   strcpy(_scopeId, scopeId.c_str());
+  MemoryManager::WriteStringOnFile(MemoryManager::SCOPE_FILENAME, scopeId);
   Serial.printf("Updated scope id to %s\n", _scopeId);
 }
 
 void AzureCommunication::SetDeviceId(String deviceId){
   strcpy(_deviceId, deviceId.c_str());
+  MemoryManager::WriteStringOnFile(MemoryManager::DEVICE_FILENAME, deviceId);
   Serial.printf("Updated device id to %s\n", _deviceId);
 }
 
 void AzureCommunication::SetDeviceKey(String deviceKey){
   strcpy(_deviceKey, deviceKey.c_str());
+  MemoryManager::WriteStringOnFile(MemoryManager::KEY_FILENAME, deviceKey);
   Serial.printf("Updated device key to %s\n", _deviceKey);
 }
 
