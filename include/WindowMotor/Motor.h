b970081220308d5563341847b5a42cffa173fff7
Daniel Bauchspiess <dbauchspiess@gmail.com>
2023-03-12 11:24:16 -0300

    Added methods to turn on and off the dc motor

--- include/WindowMotor/Motor.h
+++ include/WindowMotor/Motor.h
@@ -1,10 +1,17 @@
 #pragma once
 
+#include <Arduino.h>
+
 class Motor
 {
 public:
-    Motor();
+    Motor(uint16_t pinA, uint16_t pinB);
     ~Motor();
+
+    void SpinForward();
+    void SpinBackward();
+    void Stop();
 private:
+    uint16_t _pinA, _pinB;
 };
 
--- src/WindowMotor/Motor.cpp
+++ src/WindowMotor/Motor.cpp
@@ -1,9 +1,29 @@
 #include "WindowMotor/Motor.h"
 
-Motor::Motor()
+Motor::Motor(uint16_t pinA, uint16_t pinB)
 {
+    _pinA = pinA;
+    _pinB = pinB;
+
+    pinMode(_pinA, OUTPUT);
+    pinMode(_pinB, OUTPUT);
 }
 
 Motor::~Motor()
 {
+}
+
+void Motor::SpinForward(){
+    digitalWrite(_pinA, HIGH);
+    digitalWrite(_pinB, LOW);
+}
+
+void Motor::SpinBackward(){
+    digitalWrite(_pinA, LOW);
+    digitalWrite(_pinB, HIGH);
+}
+
+void Motor::Stop(){
+    digitalWrite(_pinA, LOW);
+    digitalWrite(_pinB, LOW);
 }
